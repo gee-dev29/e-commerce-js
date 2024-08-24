4879ba48d256f7005c20c871c8bc93ac22f4e854
donemmx <emmanuelidus@gmail.com>
2024-08-24 21:27:33 +0100

    created auth route and added extra data

--- route/cartRoute.js
+++ route/cartRoute.js
@@ -1,3 +1,10 @@
+import { checkUser } from "../middleware/checkUser";
+import { jwtVerify } from "../middleware/jwtAuthentication";
 import { router } from "../utils/routerExport";
 
-router
+router.route("/")
+    .get(jwtVerify, checkUser)
+    .post(jwtVerify, checkUser)
+
+router.route(":id")
+    .delete(jwtVerify, checkUser);
