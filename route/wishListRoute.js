dc6eeb04043e29f86b4575c47a888e7df80e52ee
donemmx <emmanuelidus@gmail.com>
2024-09-16 21:06:21 +0100

    fix

--- route/wishListRoute.js
+++ route/wishListRoute.js
@@ -2,21 +2,22 @@ import express from "express";
 import { jwtVerify } from "../middleware/jwtAuthentication.js";
 import { checkProduct } from "../middleware/checkProduct.js";
 import {
-    addItemToWishList,
-    deleteWishList,
-    updateWishList,
-    viewWishList,
+  addItemToWishList,
+  deleteWishList,
+  updateWishList,
+  viewWishList,
 } from "../controllers/wishListController.js";
 import { checkWishList } from "../middleware/checkWishList.js";
 import { findWishList } from "../middleware/FindWishList.js";
 const router = express.Router();
 
 router
-    .route("/")
-    .post(jwtVerify, checkProduct, findWishList, addItemToWishList);
+  .route("/")
+  .post(jwtVerify, checkProduct, findWishList, addItemToWishList)
+  .get(jwtVerify, checkWishList, viewWishList);
+  
 router
-    .route("/:wishListId")
-    .patch(jwtVerify, checkWishList, updateWishList)
-    .get(jwtVerify, checkWishList, viewWishList)
-    .delete(jwtVerify, checkWishList, deleteWishList);
+  .route("/:wishListId")
+  .patch(jwtVerify, checkWishList, updateWishList)
+  .delete(jwtVerify, checkWishList, deleteWishList);
 export default router;
