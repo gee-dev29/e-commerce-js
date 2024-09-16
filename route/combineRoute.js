eb6d5bff1394dac5dacb08c8cfce9c3dea8ec663
donemmx <emmanuelidus@gmail.com>
2024-09-16 21:05:55 +0100

    fix

--- route/combineRoute.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import authRoute from "./authRoute";
-const app = express.Router();
-
-module.exports.combineRoute = () => {
-    app.use("/api/v1/auth", authRoute);
-};
