6def2aef40cbe15362e1084c30d93359a12b4669
donemmx <emmanuelidus@gmail.com>
2024-09-18 04:50:32 +0100

    fix

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -28,3 +28,13 @@ export const getAllCategories = async (req, res) => {
     });
   } catch (error) {}
 };
+
+
+export const getAllColors = async (req, res) => {
+  try {
+    const result = await entity.getAllFilteredData(categoryModel, {});
+    return res.status(200).json({
+      payload: result,
+    });
+  } catch (error) {}
+};
