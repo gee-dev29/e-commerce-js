92db5e990c7235a2f9a1d2d811359a73bf1dc6e2
donemmx <emmanuelidus@gmail.com>
2024-09-18 04:26:18 +0100

    fix

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,12 +1,30 @@
+import { categoryModel } from "../model/categoryModel";
 import { entity } from "../utils/entity";
 
 export const addCategory = async (req, res) => {
   try {
-    const checkFields = entity.checkMissingFieldsInput('category', req.body)
+    const checkFields = entity.checkMissingFieldsInput("name", req.body);
     if (!checkFields.result) {
       return res.status(400).json({
         message: checkFields.message,
       });
     }
+    const category = new categoryModel({
+        name: req.body.name
+    })
+    await category.save()
+
+    return res.status(200).json({
+      message: 'successful',
+    });
+  } catch (error) {}
+};
+
+export const getAllCategories = async (req, res) => {
+  try {
+    const result = entity.getAllFilteredData(categoryModel, {});
+    return res.status(200).json({
+      payload: result,
+    });
   } catch (error) {}
 };
