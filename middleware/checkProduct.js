ee637d0888c36780a1b3365cd8541d5547edc6f1
donemmx <emmanuelidus@gmail.com>
2024-09-21 17:10:19 +0100

    fix

--- middleware/checkProduct.js
+++ middleware/checkProduct.js
@@ -1,21 +1,25 @@
 import { productModel } from "../model/productModel.js";
+import { entity } from "../utils/entity.js";
 
 export const checkProduct = async (req, res, next) => {
-    let productId;
-    if (req.params.productId) {
-        productId = req.params.productId;
-    } else if (req.body.productId) {
-        productId = req.body.productId;
-    }
-    if (!productId) {
-        return res.status(400).json({ message: "Product Id is required" });
-    }
-    const product = await productModel.findById({ _id: productId });
-    console.log(product)
-    if (!product) {
-        return res.status(404).json({ message: "product not found" });
-    }
-    req.productId = productId;
-    req.product = product;
-    next();
+  let productId = req.params.productId || req.body.productId
+  // if (req.params?.productId) {
+  //   productId = req.params?.productId;
+  // } else if (req.body?.productId) {
+  //   productId = req.body?.productId;
+  // }
+
+
+  if (!entity.isValidUUID(productId)) {
+    return res.status(400).json({ message: "Product Id is required" });
+  }
+  const product = await entity.getAllFilteredData(productModel, {
+    _id: productId,
+  });
+  if (!product) {
+    return res.status(404).json({ message: "product not found" });
+  }
+  req.productId = productId;
+  req.product = product[0];
+  next();
 };
--- utils/entity.js
+++ utils/entity.js
@@ -116,6 +116,11 @@ const checkMissingFieldsInput = (requiredFields, requestBody) => {
     };
 };
 
+const isValidUUID = (id) => {
+    const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+    return uuidRegex.test(id);
+  };
+
 export const entity = {
     encryptPassword,
     decryptPassword,
@@ -130,4 +135,5 @@ export const entity = {
     encryptData,
     decryptData,
     generateOtp,
+    isValidUUID
 };
