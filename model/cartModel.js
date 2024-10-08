0f03bf78531b94f1d9ec276d940c336666e31678
donemmx <emmanuelidus@gmail.com>
2024-10-08 10:12:59 +0100

    fix

--- model/cartModel.js
+++ model/cartModel.js
@@ -1,37 +1,35 @@
 import mongoose from "mongoose";
 
 const cartSchema = new mongoose.Schema(
-    {
-        creatorId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "user",
-            required: true,
-        },
-        productIds: [
-            {
-                product: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "product",
-                    required: true,
-                },
-                items: {
-                    quantity: {
-                        type: Number,
-                        required: true,
-                    },
-                    color: {
-                        type: String,
-                        required: true,
-                    },
-                    size: {
-                        type: String,
-                        required: true,
-                    },
-                },
-            },
-        ],
+  {
+    creatorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
     },
-    { timestamps: true }
+    productIds: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+        color: {
+          type: String,
+          required: true,
+        },
+        size: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
 );
 
 export const cartModel = mongoose.model("cart", cartSchema);
