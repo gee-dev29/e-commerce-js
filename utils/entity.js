4552ccae2ff70eb1c021d50ac8764f9b6c2ca12c
gee-dev29 <maduegodwin2605@gmail.com>
2024-10-15 13:03:05 +0100

    fix

--- utils/entity.js
+++ utils/entity.js
@@ -8,174 +8,176 @@ import mongoose from "mongoose";
 
 // Encrypt function
 function encryptData(text, key) {
-  const cipher = crypto.createCipher("aes-256-cbc", key);
-  let encrypted = cipher.update(text, "utf8", "hex");
-  encrypted += cipher.final("hex");
-  return encrypted;
+    const cipher = crypto.createCipher("aes-256-cbc", key);
+    let encrypted = cipher.update(text, "utf8", "hex");
+    encrypted += cipher.final("hex");
+    return encrypted;
 }
 
 // Decrypt function
 function decryptData(encryptedText, key) {
-  const decipher = crypto.createDecipher("aes-256-cbc", key);
-  let decrypted = decipher.update(encryptedText, "hex", "utf8");
-  decrypted += decipher.final("utf8");
-  return decrypted;
+    const decipher = crypto.createDecipher("aes-256-cbc", key);
+    let decrypted = decipher.update(encryptedText, "hex", "utf8");
+    decrypted += decipher.final("utf8");
+    return decrypted;
 }
 
 const updateDataById = async (id, payload, model) => {
-  return await model.findByIdAndUpdate(id, payload, { new: true });
+    return await model.findByIdAndUpdate(id, payload, { new: true });
 };
 
 const updateArrayOfData = async (id, payload, model) => {
-  return await model.updateOne(id, { $push: payload });
+    return await model.updateOne(id, { $push: payload });
 };
 
 const deleteDataById = async (id, model) => {
-  return await model.findByIdAndDelete(id);
+    return await model.findByIdAndDelete(id);
 };
 
 const updateUserByEmail = async (email, payload, model) => {
-  return await model.updateOne({ email: email }, payload, { new: true });
+    return await model.updateOne({ email: email }, payload, { new: true });
 };
 
 // encrypt user password
 const encryptPassword = async (password) => {
-  const salt = await bcrypt.genSalt(10);
-  const HashPassword = await bcrypt.hash(password, salt);
-  return HashPassword;
+    const salt = await bcrypt.genSalt(10);
+    const HashPassword = await bcrypt.hash(password, salt);
+    return HashPassword;
 };
 
 // decrypt password
 const decryptPassword = async (password, user) => {
-  return await bcrypt.compare(password, user);
+    return await bcrypt.compare(password, user);
 };
 
 // Document upload
 const checkUploadDoc = async (body) => {
-  const { file } = body;
-  if (file == "") {
-    return null;
-  }
-  return body;
+    const { file } = body;
+    if (file == "") {
+        return null;
+    }
+    return body;
 };
 
 const jwtSign = (id) => {
-  const token = jwt.sign(
-    {
-      userId: id,
-    },
-    process.env.JWT_SECRET,
-    {
-      expiresIn: "2hr",
-    }
-  );
-  return token;
+    const token = jwt.sign(
+        {
+            userId: id,
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: "2hr",
+        }
+    );
+    return token;
 };
 
 // generate Otp
 
 const generateOtp = () => {
-  const value = Math.random().toString().substr(2, 4);
-  const expiresIn = new Date(Date.now() + 10 * 60 * 1000);
-  return { otp: value, expiresIn: expiresIn };
+    const value = Math.random().toString().substr(2, 4);
+    const expiresIn = new Date(Date.now() + 10 * 60 * 1000);
+    return { otp: value, expiresIn: expiresIn };
 };
 
 const getAllFilteredData = async (model, filter) => {
-  const data = model.find(filter).sort({ createdAt: -1 });
-  return data;
+    const data = model.find(filter).sort({ createdAt: -1 });
+    return data;
 };
 
 const checkMissingFieldsInput = (requiredFields, requestBody) => {
-  const missingOrEmptyFields = [];
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const missingOrEmptyFields = [];
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-  requiredFields.forEach((field) => {
-    const value = requestBody[field];
-    if (
-      !requestBody.hasOwnProperty(field) ||
-      value === null ||
-      value === undefined ||
-      value === ""
-    ) {
-      missingOrEmptyFields.push(field);
-    } else if (field === "email" && !emailRegex.test(value)) {
-      missingOrEmptyFields.push(`${field} (invalid email)`);
+    requiredFields.forEach((field) => {
+        const value = requestBody[field];
+        if (
+            !requestBody.hasOwnProperty(field) ||
+            value === null ||
+            value === undefined ||
+            value === ""
+        ) {
+            missingOrEmptyFields.push(field);
+        } else if (field === "email" && !emailRegex.test(value)) {
+            missingOrEmptyFields.push(`${field} (invalid email)`);
+        }
+    });
+
+    if (missingOrEmptyFields.length > 0) {
+        return {
+            result: false,
+            message: `Missing required fields: ${missingOrEmptyFields.join(
+                ", "
+            )}`,
+        };
     }
-  });
 
-  if (missingOrEmptyFields.length > 0) {
     return {
-      result: false,
-      message: `Missing required fields: ${missingOrEmptyFields.join(", ")}`,
+        result: true,
     };
-  }
-
-  return {
-    result: true,
-  };
 };
 
 const isValidUUID = (id) => {
-  const uuidRegex =
-    /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
-  return uuidRegex.test(id);
+    const uuidRegex =
+        /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+    return uuidRegex.test(id);
 };
 
 const isValidObjectId = (id) => {
-  const isValid = mongoose.Types.ObjectId.isValid({ id: id });
-  return isValid;
+    const isValid = mongoose.Types.ObjectId.isValid({ id: id });
+    return isValid;
 };
 
 const getPaginatedData = async (model, filter, skip, limit) => {
-  const data = await model.find(filter).limit(limit).skip(skip);
-  const totalRecords = data.length;
-  return { data, totalRecords };
+    const data = await model.find(filter).limit(limit).skip(skip);
+    const totalRecords = data.length;
+    return { data, totalRecords };
 };
 const getPaginatedDataWithPopulate = async (
-  model,
-  filter,
-  skip,
-  limit,
-  path,
-  selectedModel
+    model,
+    filter,
+    skip,
+    limit,
+    path,
+    selectedModel
 ) => {
-  const data = await model
-    .find(filter)
-    .populate({
-      path: path,
-      model: selectedModel,
-    })
-    .limit(limit)
-    .skip(skip);
-  const totalRecords = data.length;
-  return { data, totalRecords };
+    const data = await model
+        .find(filter)
+        .populate({
+            path: path,
+            model: selectedModel,
+        })
+        .limit(limit)
+        .skip(skip);
+    const totalRecords = data.length;
+    return { data, totalRecords };
 };
 
 const generateOrderNumber = () => {
-  const prefix = "#FWB";
+    const prefix = "#FWB";
 
-  const randomNumber = Math.floor(100000000 + Math.random() * 900000000);
+    const randomNumber = Math.floor(100000000 + Math.random() * 900000000);
 
-  return `${prefix}${randomNumber}`;
+    return `${prefix}${randomNumber}`;
 };
 
 export const entity = {
-  encryptPassword,
-  getPaginatedDataWithPopulate,
-  getPaginatedData,
-  decryptPassword,
-  jwtSign,
-  getAllFilteredData,
-  checkUploadDoc,
-  updateUserByEmail,
-  checkMissingFieldsInput,
-  updateDataById,
-  deleteDataById,
-  updateArrayOfData,
-  encryptData,
-  decryptData,
-  generateOtp,
-  isValidUUID,
-  isValidObjectId,
-  generateOrderNumber
+    encryptPassword,
+    getPaginatedDataWithPopulate,
+    getPaginatedData,
+    decryptPassword,
+    jwtSign,
+    getAllFilteredData,
+    checkUploadDoc,
+    updateUserByEmail,
+    checkMissingFieldsInput,
+    updateDataById,
+    deleteDataById,
+    updateArrayOfData,
+    encryptData,
+    decryptData,
+    generateOtp,
+    isValidUUID,
+    isValidObjectId,
+    generateOrderNumber,
 };
