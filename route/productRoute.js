27f45d20129f1582e625890c7a184e719be1f375
donemmx <emmanuelidus@gmail.com>
2024-10-26 22:33:24 +0100

    fix

--- route/productRoute.js
+++ route/productRoute.js
@@ -1,10 +1,10 @@
 import express from "express";
 import {
-    createProduct,
-    deleteProduct,
-    searchProduct,
-    viewProduct,
-    viewProducts,
+  createProduct,
+  deleteProduct,
+  searchProduct,
+  viewProduct,
+  viewProducts,
 } from "../controllers/productController.js";
 import { jwtVerify } from "../middleware/jwtAuthentication.js";
 import { checkProduct } from "../middleware/checkProduct.js";
@@ -13,11 +13,11 @@ import { checkUser } from "../middleware/checkUser.js";
 const router = express.Router();
 
 router
-    .route("/")
-    .post(jwtVerify, createProduct)
-    .get(viewProducts)
-    .delete(jwtVerify, checkUser, superAdminRoleCheck, deleteProduct);
-router.route("/search/product").get(jwtVerify, checkUser, searchProduct);
+  .route("/")
+  .post(jwtVerify, createProduct)
+  .get(viewProducts)
+  .delete(jwtVerify, checkUser, superAdminRoleCheck, deleteProduct);
+router.route("/search/product").get(searchProduct);
 
 router.route("/:productId").get(checkProduct, viewProduct);
 
